e3d3a1738450d72b5f6bf8fcb425a9caf54cd73d
albertklik <paulo.albertklik@gmail.com>
2016-04-19 18:01:17 -0300

    Dynamic setup layout and organization of resources

--- BattleChips/src/battlechips/Gui_TableBlock.java
+++ BattleChips/src/battlechips/Gui_TableBlock.java
@@ -5,6 +5,11 @@
  */
 package battlechips;
 
+import javax.swing.ImageIcon;
+import java.awt.Image;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
 /**
  *
  * @author berto
@@ -13,12 +18,48 @@ public class Gui_TableBlock extends javax.swing.JButton {
     
     private int position[] = new int[2];
     private int Size;
+    private int orient;
     
     
+    
+    
+    
+    
+    public Gui_TableBlock(int x, int y, int size) {
+        position[0] = x;
+        position[1] = y;
+        Size = size;
+        
+        initComponent();
+    }
     
     
     public void initComponent() {
         
+        java.awt.Dimension Bsize = new java.awt.Dimension(Size, Size);
+        setSize(Bsize);
+        setMaximumSize(Bsize);
+        setMinimumSize(Bsize);
+        setPreferredSize(Bsize);
+        setFocusable(false);
+        
+        
+         
+        setIcon(reSize(resources.r.BlockDefault));
+        
+    }
+    
+    
+    public void setIconChip(int tipo, int orient) {
+       
+    }
+    
+    
+    private ImageIcon reSize(ImageIcon img) {
+        
+        img.setImage(img.getImage().getScaledInstance(Size, Size, Image.SCALE_DEFAULT));
+       
+        return img;
     }
     
     
---
+++ BattleChips/src/resources/r.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package resources;
+import javax.swing.ImageIcon;
+/**
+ *
+ * @author berto
+ */
+public class r {
+    public static ImageIcon BlockDefault = new ImageIcon("/resources/Default_BackgroundTile.png");
+}
